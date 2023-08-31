db2873636de67da3c520f9243121f2e7e33652ae
ValenMir18 <mpvalu17@gmail.com>
2023-08-31 12:06:00 -0500

    Autocompletado, textfiel y modal

---
+++ src/pages/Registro.jsx
@@ -0,0 +1,14 @@
+import Textfield from "../components/TextField"
+import AutoComplete from "../components/AutoComplete"
+import ModalTyC from "../components/ModalTyC"
+
+export default function Registro(){
+    return (
+    <div>
+        <Textfield />
+        <AutoComplete />
+        <ModalTyC />
+    </div>
+    
+    )
+}
