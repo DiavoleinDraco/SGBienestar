0e964e63e38bfd79fda5d775fd2fa8647d7cd1a6
LuisaArR <luisaarchila123@gmail.com>
2023-09-02 08:01:44 -0500

    creacion de facha

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,12 +1,18 @@
 import ComSelect from "../components/styles/ComSelect.jsx";
 
+
 export default function Registro () {
   return (
+    <div>
+
+   
     <div>
     <ComSelect nombre= "Tipo de documento" items={["C.C", "T.I", "PA","C.E"]}/>
     <ComSelect nombre= "Género" items={["Masculino","Femenino","Otro"]} />
     <ComSelect nombre= "Tipo de Sangre" items={["A+","O+","B+","AB+","A-","O-","B-","AB-"]} />
     <ComSelect nombre= "Rol" items={["Instructor","Aprendiz","Administrador"]} />
+    </div>
+
     </div>
   );
 }
