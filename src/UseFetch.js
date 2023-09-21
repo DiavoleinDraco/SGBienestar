f210177000f40afa8a679f79219382827eb485b9
LuisaArR <luisaarchila123@gmail.com>
2023-09-21 08:00:09 -0500

    validacion de error de correo existente

--- src/UseFetch.js
+++ src/UseFetch.js
@@ -1,4 +1,3 @@
-
 const UrlApi= "https://proyecto-backend-sgbienestar.onrender.com"
 
 export default async function get (pat){
@@ -15,18 +14,30 @@ try{
 
 }
  
-export async function post (pat,data){
-  try{
-    
-    const response= await fetch(UrlApi+pat,{method:"POST",headers: {
-      'Content-Type': 'application/json', 
-    }, body:JSON.stringify(data)})
-    const data1= await response.json()
-    return data1
-  
-  } catch(error){
-    console.log('No se encontro la informacion', error)
-  
+export async function post(pat, data) {
+  try {
+    const response = await fetch(UrlApi + pat, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      
+      if (response.status === 'ERROR: El Usuario ya existe') {
+        
+        throw new Error("El correo electrónico ya está en uso. Por favor, elija otro.");
+      } else {
+        throw new Error(`Error al realizar la solicitud: ${response.status}`);
+      }
+    }
+
+    const data1 = await response.json();
+    return data1;
+  } catch (error) {
+    console.error('Error al realizar la solicitud:', error);
+    throw error;
   }
-  
-  }
+}
---
+++ src/pages/RecuperarContrasena.jsx
@@ -0,0 +1,8 @@
+import InputCorreo from "../components/ComantCorreo/ComantCorreo";
+import ButtonContraseña from "../components/ButtonContraseña/ButtonContraseña";
+
+
+export default function RecuperarContrasena () {
+
+
+}
