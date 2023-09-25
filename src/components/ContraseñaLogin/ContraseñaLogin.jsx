0130f3fced51172d39a46315b72ad5c51af9cbaf
Angel777P <angel.mkv777@gmail.com>
2023-09-25 07:52:17 -0500

    Arreglos de diseño

--- src/components/ContraseñaLogin/ContraseñaLogin.jsx
+++ src/components/ContraseñaLogin/ContraseñaLogin.jsx
@@ -1,69 +1,70 @@
-import React, { useState } from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import { Input } from '@material-ui/core';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import { Visibility } from '@material-ui/icons';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import React, { useState } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import { Input } from "@material-ui/core";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import { Visibility } from "@material-ui/icons";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
-  export default function ContraseñaLogin({ nombre, onChange, required }){
-    const [passwordError, setPasswordError] = useState(false);
-    const [values, setValues] = useState({password: '', showPassword: false, });
+export default function ContraseñaLogin({ nombre, onChange, required }) {
+  const [passwordError, setPasswordError] = useState(false);
+  const [values, setValues] = useState({ password: "", showPassword: false });
 
-    const handleChange = (prop) => (event) => {
-      setValues({ ...values, [prop]: event.target.value })
-      const fieldValue = event.target.value
-      onChange(fieldValue)
-    };
-
-    const handleClickShowPassword = () => {
-      setValues({ ...values, showPassword: !values.showPassword });
-    };
-
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-
-    const handleBlur = () => {
-      if (required && values.password === '') {
-        setPasswordError(true);
-      } else {
-        setPasswordError('');
-      }
-    }
-
-    return (
-
-        
-        <FormControl className='conts' sx={{ m: 1, }} variant="standard">
-          <InputLabel className='contraseña' htmlFor="standard-adornment-password">{nombre}</InputLabel>
-          <Input
-            id="standard-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            onBlur={handleBlur}
-            label={nombre}
-            error={Boolean(passwordError)}
-            helperText={passwordError}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  style={{position: 'relative', right: '40px'}}
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-          {(passwordError) && (<p style={{ color: 'red', position: 'relative', left: '-26px' }}>Este campo es obligatorio</p>)}
-        </FormControl>
-
-  
-    );
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+    const fieldValue = event.target.value;
+    onChange(fieldValue);
   };
-  
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleBlur = () => {
+    if (required && values.password === "") {
+      setPasswordError(true);
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  return (
+    <FormControl className="conts" sx={{ m: 1 }} variant="standard">
+      <InputLabel className="contraseña" htmlFor="standard-adornment-password">
+        {nombre}
+      </InputLabel>
+      <Input
+        id="standard-adornment-password"
+        type={values.showPassword ? "text" : "password"}
+        value={values.password}
+        onChange={handleChange("password")}
+        onBlur={handleBlur}
+        label={nombre}
+        error={Boolean(passwordError)}
+        helperText={passwordError}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              style={{ position: "relative", right: "16px" }}
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+            >
+              {values.showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+      {passwordError && (
+        <p style={{ color: "red", position: "relative", left: "-14px" }}>
+          Este campo es obligatorio
+        </p>
+      )}
+    </FormControl>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Home.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
-      <Link to="/Registro">Registro</Link>
-      <br />
-      <Link to="/login">Login</Link>
-    </div>
+    <nav className="navbar">
+      <div className="logo">
+        <h1>Home</h1>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/Registro">Registro</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
