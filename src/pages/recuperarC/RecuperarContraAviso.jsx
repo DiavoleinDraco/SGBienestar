8492a0275f442b15e554678ba8cb24b97f851397
Angel777P <angel.mkv777@gmail.com>
2023-09-29 09:22:25 -0500

    Actualizacion de diseño en Aviso de recuperar contraseña

--- src/pages/recuperarC/RecuperarContraAviso.jsx
+++ src/pages/recuperarC/RecuperarContraAviso.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import miimagen from "../../pages/imagenes/sena-bienestar.png";
 import { useNavigate } from 'react-router-dom';
+import './RecuperarContraAviso.css';
 
 
 
 export default function RecuperacionContraAviso() {
-const [cambioExitoso, setCambioExitoso] = useState(false);
+  const [cambioExitoso, setCambioExitoso] = useState(false);
 
   const navegacion = useNavigate();
   if (cambioExitoso) {
@@ -15,18 +16,30 @@ const [cambioExitoso, setCambioExitoso] = useState(false);
 
 
   return (
-    <div>
+    <div className="contenedor-padre">
       <figure>
-        <img className="foto" src={miimagen} alt="sena-imagen" />
+        <img className="foto-recontra" src={miimagen} alt="sena-imagen" />
       </figure>
-      <div className="father-conf">
+      <div className="contenedor-info-recontra">
         <div>
           <h1>
             Cambio de contraseña exitoso
           </h1>
           <div>
-          <Link to="/login">volver a iniciar secion</Link>
-        </div>
+            <p className="texto-contra-re">
+              <b>
+                Estimado usuario. 
+                <br />
+                <br />
+                !Su contraseña ha sido cambiada con éxito¡ 
+                <br />
+                Haz clic en el siguiente enlace: <Link className="custom-link" to="/login">Iniciar sesion</Link>, para acceder a tú cuenta.
+                <br />
+                <br />
+                ¡Bienvenido de vuelta!
+              </b>
+            </p>
+          </div>
         </div>
       </div>
     </div>
