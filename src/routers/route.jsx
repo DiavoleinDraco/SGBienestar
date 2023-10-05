1b9b9bf545e6bce59ee222818a7b3b8aee367990
LuisaArR <luisaarchila123@gmail.com>
2023-10-05 12:03:56 -0500

    actualizacion de sanciones

--- src/routers/route.jsx
+++ src/routers/route.jsx
@@ -5,10 +5,9 @@ import Registro from '../pages/registro/Registro'
 import Login from '../pages/login/Login';
 import Autenticacion from '../pages/autenticacion/Autenticacion';
 import RecuperarContrasena from '../pages/recuperarC/RecuperarContrasena.jsx';
-import RecuperacionContraAviso from '../pages/recuperarC/RecuperarContraAviso';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import NavTabs from '../pages/NavTabs/NavTabs';
-import Sanciones from '../pages/Sanciones/Sanciones';
+import Sanciones from '../pages/sanciones/sanciones';
 
 function requireAuth({ children }) {
   const token = localStorage.getItem('token');
@@ -27,7 +26,6 @@ export function LasRutas() {
         <Route path="/login" element={<Login />} />
         <Route path="/auth/:userId" element={<Autenticacion />} />
         <Route path="/RecuperarContrasena" element={<RecuperarContrasena/>} />
-        <Route path="/RecuperacionContraAviso" element={<RecuperacionContraAviso/>} />
         <Route path="/admin" element={<Dashboard/>} />
         <Route path="/tabla" element={<NavTabs/>} />
         <Route path="/sanciones" element={<Sanciones/>} />
