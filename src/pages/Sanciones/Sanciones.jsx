12216d7237b50a9596720fbd665764362ed49d10
LuisaArR <luisaarchila123@gmail.com>
2023-10-06 10:07:38 -0500

    actualizacion de sanciones

--- src/pages/Sanciones/Sanciones.jsx
+++ src/pages/Sanciones/Sanciones.jsx
@@ -1,41 +1,18 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
 import MultipleSelect from '../../components/MultipleSelect/MultipleSelect';
 import Textfield from '../../components/Textfield/Textfield';
 import { useState, useEffect } from "react";
+import Menu from '../../components/menu/Menu';
+import './Sanciones.css'
 
 
-const drawerWidth = 380;  //MODIFICA EL TAMAÑO DE LA BARRA LATERAL
-
-export default function Sanciones(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [selectedOptions, setSelectedOptions] = useState([]);  //ESTADOS DEL COMPONENTE DE OPCIONES
+export default function Sanciones () {
+const [selectedOptions, setSelectedOptions] = useState([]);
 
 
+//OPCIONES DEL COMPONENTE SELECT//
 
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-
-
-
-  //____________________________________________________
-
-  //OPCIONES DEL COMPONENTE SELECT//
-
-
-  const selectOptions = [
+const selectOptions = [
   { value: 'opcion1', label: 'Incumplimiento de la entrega del implemento' },
   { value: 'opcion2', label: 'Daño del implemento deportivo' },
   { value: 'opcion3', label: 'Faltas recurrentes por la entrega tardia del implemento ' },
@@ -66,9 +43,14 @@ export default function Sanciones(props) {
 
 //___________________________________________________
 
-const drawer = (
-  <div>
+
+return (
+  <div className='container-sanciones'>
+    
+    <Menu></Menu>
+
     <p>Sanciones </p>
+
     <Textfield
       className="son-codigo"
       name="ID"
@@ -78,82 +60,27 @@ const drawer = (
       selectedOptions={selectedOptions}
       onChange={handleChange}
     />
+     
+     <Textfield
+      className="son-codigo"
+      name="Nueva Sancion"
+    />
+
+
+
   </div>
 );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
-
-  //______________________________________________________________________
 
 
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: `calc(100% - ${drawerWidth}px)`, //PARA AJUTAR EL HANCHO DE LA PAGINA PRINCIPAL
-        }}
-      >
-        {/* LLAMAR AQUI AL CONTENIDO GENERAL*/}
-      </Box>
-    </Box>
-  );
+
+
+
+
+
+
+
+
 }
 
-Sanciones.propTypes = {
-  window: PropTypes.func,
-};
 
