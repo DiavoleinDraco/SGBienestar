e10dc6c50798eda3ee4b1055e11fb726afedabec
DiavoleinDraco <pedroluispaterninaargumedo10.1@gmail.com>
2023-10-18 10:09:11 -0500

    Adelanto 2

--- src/components/Tablacorreos/Tablacorreos.jsx
+++ src/components/Tablacorreos/Tablacorreos.jsx
@@ -1,26 +1,38 @@
-import { useState, useEffect } from "react";
-import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
-import get from "../../UseFetch";
-import { Link } from 'react-router-dom'; // Importa el componente Link de React Router
-import "./Tablacorreo.css";
-export default function DataGridDemo() {
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useNavigate } from "react-router-dom";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import get from '../../UseFetch';
+
+export default function DataGridProDemo() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [data, setData] = useState([]);
-  const [rows, setRows] = useState([]); // Estado para las filas
+  const [rows, setRows] = useState([]);
+  const navigate = useNavigate(); // Usamos useNavigate para la navegación
 
   useEffect(() => {
-    // Hacer la petición a la API
     get("/mail")
       .then((data) => {
-        // Almacenar los datos en el estado
         setData(data);
 
-        // Crear las filas con los IDs, correo, mensaje y asunto
         const rowsData = data.map((item) => ({
-          id: item._id,
+          _id: item._id,
           correo: item.correo,
-          mensaje: item.mensaje,
           asunto: item.asunto,
+          mensaje: item.mensaje,
         }));
         setRows(rowsData);
       })
@@ -29,33 +41,72 @@ export default function DataGridDemo() {
       });
   }, []);
 
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    { field: 'correo', headerName: 'Correo', width: 150, editable: true },
-    { field: 'mensaje', headerName: 'Mensaje', width: 300, editable: true },
-    { field: 'asunto', headerName: 'Asunto', width: 200, editable: true },
-    {
-      field: 'verDetalles',
-      headerName: 'Ver Detalles',
-      width: 150,
-      renderCell: (params) => (
-        <Link to={`/detalles/${params.row.id}`}>Ver Detalles</Link>
-      ),
-    },
-  ];
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleRowClick = (_id) => {
+    // Usamos navigate para redirigir
+    navigate(`/detalle/${_id}`);
+  };
 
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
-   <DataGrid
-  rows={rows}
-  columns={columns}
-  onRowClick={(params) => {
-    // Redirige a la página de detalles al hacer clic en cualquier parte de la fila
-    window.location.href = `/detalles/${params.row.id}`;
-  }}
-  getRowClassName={(params) => "clickable-row"}
-/>
+    <TableContainer component={Paper}>
+      <Table aria-label="custom pagination table">
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row, index) => (
+            <TableRow
+              key={index}
+              onClick={() => handleRowClick(row._id)}
+              style={{ cursor: 'pointer' }}
+            >
+              <TableCell component="th" scope="row">
+                {row.correo}
+              </TableCell>
+              <TableCell align="right">
+                {row.asunto}
+              </TableCell>
+              <TableCell align="right">
+                {row.mensaje}
+              </TableCell>
+            </TableRow>
+          ))}
 
-  </Box>
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={3} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+             
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
   );
 }
