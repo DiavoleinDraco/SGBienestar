64f73409597deebbb00936c73d7d26efaf6ee5d1
ValenMir18 <mpvalu17@gmail.com>
2023-11-28 11:19:00 -0500

    TablaInventario terminado y gran avance de implementos en aprendiz

---
+++ src/components/Almacenar_Imple/Almacenar_Imple.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import Box from '@mui/material/Box';
+import Badge from '@mui/material/Badge';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+export default function Almacenar_Imple({ cantidadDisponible, onCountChange, isImplementoSelected }) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Restablecer el valor a 0 cuando se deselecciona el implemento
+    if (!isImplementoSelected) {
+      setCount(0);
+      onCountChange(0);
+    }
+  }, [isImplementoSelected]);
+
+  const handleDecrease = () => {
+    // Verificar que la cantidad no sea menor que 1
+    if (count >= 1) {
+      setCount(count - 1);
+      onCountChange(count - 1);
+    }
+  };
+
+  const handleIncrease = () => {
+    // Verificar que la cantidad no supere la cantidad disponible
+    if (count < cantidadDisponible) {
+      setCount(count + 1);
+      onCountChange(count + 1);
+    }
+  };
+
+
+  return (
+    <Box
+      sx={{
+        color: 'action.active',
+        display: 'flex',
+        flexDirection: 'column',
+        '& > *': {
+          marginBottom: 2,
+        },
+        '& .MuiBadge-root': {
+          marginRight: 4,
+        },
+      }}
+    >
+      <div>
+        
+        <ButtonGroup>
+        <input type="text" value={count} />
+          <Button aria-label="reduce" onClick={handleDecrease} disabled={!isImplementoSelected}>
+            <RemoveIcon fontSize="small" />
+          </Button>
+          <Button aria-label="increase" onClick={handleIncrease} disabled={!isImplementoSelected}>
+            <AddIcon fontSize="small" />
+          </Button>
+        </ButtonGroup>
+      </div>
+    </Box>
+  );
+}
